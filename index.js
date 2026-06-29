0ab387758a302d5143f1ae4c0b1281537b02c2a1
2df-code <bouchelitazizida@gmail.com>
2026-06-29 10:00:15 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const mineflayer = require('mineflayer')
+const http = require('http')
+
+http.createServer((req, res) => {
+  res.end('Bot is running!')
+}).listen(3000)
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'Kingdom2.aternos.me',
+    port: 45630,
+    username: 'KeepAliveBot',
+    version: '1.21.6',
+    auth: 'offline'
+  })
+
+  bot.on('spawn', () => {
+    console.log('✅ البوت دخل السيرفر!')
+    setInterval(() => {
+      bot.setControlState('jump', true)
+      setTimeout(() => bot.setControlState('jump', false), 500)
+    }, 60000)
+  })
+
+  bot.on('kicked', () => {
+    console.log('❌ طُرد! إعادة اتصال...')
+    setTimeout(createBot, 30000)
+  })
+
+  bot.on('error', () => {
+    setTimeout(createBot, 30000)
+  })
+
+  bot.on('end', () => {
+    setTimeout(createBot, 30000)
+  })
+}
+
+createBot()
